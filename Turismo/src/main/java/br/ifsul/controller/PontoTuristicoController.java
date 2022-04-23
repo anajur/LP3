c5d289593de8fb3a41980055a0286b3a33e80b57
Mel Viana <melevaldt@gmail.com>
2022-04-23 15:13:25 -0300

    Atualizado com cadastro de pontos turísticos

--- Turismo/src/main/java/br/ifsul/controller/PontoTuristicoController.java
+++ Turismo/src/main/java/br/ifsul/controller/PontoTuristicoController.java
@@ -1,5 +1,21 @@
 package br.ifsul.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ifsul.dto.PontoTuristico;
+import br.ifsul.service.PontoTuristicoService;
+
+@RestController
+@RequestMapping("/pontoturistico")
 public class PontoTuristicoController {
+    private PontoTuristicoService service;
+
+    @PostMapping("/adicionar")
+    void adicionarPontoTuristico(@RequestBody PontoTuristico pontoturistico) {
+        service.adicionarPontoTuristico(pontoturistico);
+    }
 
 }
